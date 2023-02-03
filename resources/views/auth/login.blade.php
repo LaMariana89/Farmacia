8bdbe121e1c3e5788cbbcec69c461ed20a2b931a
LaMariana88 <sanchez.medina.mariana.1acbtis@gmail.com>
2023-02-02 18:53:41 -0600

    es el primer commit para el primer branch Please enter the commit message for your changes. Lines starting

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,13 +4,13 @@
 
 @section('content')
 
-<div class="block mx-auto my-12 p-8 bg-slate-50 w-1/3 border border-slate-50 rounded-lg shadow-lg"> 
+<div class="block mx-auto my-12 p-8 bg-slate-50 w-1/3 border border-slate-50 rounded-lg shadow-lg">
     <div class="rounded-lg">
         <img class="w-2/3 block mx-auto" src="{!! asset('farmaLive.png') !!}">
     </div>
     <form class="mt-4" method="POST" action="">
     @csrf
-        
+
         <input type="email" class="border border-gray-100 rounded-md bg-gray-100 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white"
         placeholder="Email" id="email" name="email">
 
@@ -22,10 +22,10 @@
         @enderror
 
         <button type="submit" id="navAction" class="mx-auto lg:mx-0 hover:underline bg-purple-200 text-gray-800 font-bold rounded-full w-full mt-4 lg:mt-0 py-4 px-20 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:bg-cyan-100 duration-300 ease-in-out">Send</button>
-    
+
     </form>
     <div class="tracking-wide underline w-1/3 block mx-auto">
-        <a href="{{route('register.index')}}">Registrate ¡¡Ya!!</a>
+        <a href="{{route('register.index')}}">Registrate ¡¡Ya!! Con cambios</a>
     </div>
 </div>
 @endsection
